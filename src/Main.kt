f836c8b4654e2b942f903e6e0bc09a14b70ba957
Yousef Alothman <yousefoalothman@gmail.com>
2025-02-16 18:27:02 +0300

    Exercise Completed

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,23 @@
+class Book (var title: String){
+
+    //var title: String = ""
+    var author: String = ""
+    var yearPublished: Int = 0
+
+    fun getBookInfo(){
+
+        println("$title by $author was publish in $yearPublished")
+
+    }
+
+}
 fun main() {
-    println("Hello World!")
+
+    val myBook = Book("Objects First with Java")
+
+    //myBook.title = "Objects First with Java"
+    myBook.author = "David J. Barnes"
+    myBook.yearPublished = 2016
+    myBook.getBookInfo()
+
 }
